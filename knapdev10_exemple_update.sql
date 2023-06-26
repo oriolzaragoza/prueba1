712c0604d2ae1b8024e2048906da445534453ae7
oriolzaragoza <oriol.zaragoza@bomigroup.com>
2023-06-26 12:42:47 +0200

    second changes

---
+++ knapdev10_exemple_update.sql
@@ -0,0 +1,18 @@
+declare
+l_cnt number := 0;
+
+begin
+
+select count('x')
+  into l_cnt
+  from KNAPDEV10 k
+  where k.status = '85';
+
+  if l_cnt > 0 then
+    dbms_output.put_line ('En total updateamos '||l_cnt||'');
+
+    update knapdev10 k
+    set k.status = '90'
+    where k.status = '85';
+  end if;
+end;
